80422a3a6ef9a3ae430dee58a32fdcc8fa735993
Matthew Poetker <matt.pworks@gmail.com>
2017-02-12 00:34:14 -0600

    Second try at updating the migration file.

--- db/migrate/20161221221430_create_events.rb
+++ db/migrate/20161221221430_create_events.rb
@@ -9,8 +9,6 @@ class CreateEvents < ActiveRecord::Migration[5.0]
       t.belongs_to :category, index: true
       t.timestamps
     end
-    add_column :events, :host_id, :integer
-    add_column :events, :category_id, :integer
     add_index :events, :host_id
     add_index :events, :category_id
 
