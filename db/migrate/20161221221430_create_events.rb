f3b8a7e52a39ee8b5b4c766c83000b81fa030920
Matthew Poetker <matt.pworks@gmail.com>
2017-02-12 00:29:45 -0600

    changed events migration file from change_table to create_table

--- db/migrate/20161221221430_create_events.rb
+++ db/migrate/20161221221430_create_events.rb
@@ -1,6 +1,6 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
-    change_table :events do |t|
+    create_table :events do |t|
       t.string :name
       t.string :date
       t.string :time
